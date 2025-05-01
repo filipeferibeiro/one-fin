fe04330a68d8cff778f50017f2240d29068eb048
Filipe Fernandes <filipeferibeiro@gmail.com>
2025-05-01 11:44:39 -0300

    feat: add logout to profile

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,8 +1,44 @@
-import { View, Text } from 'react-native';
+import { auth } from '@/services/firebaseConfig';
+import { useRouter } from 'expo-router';
+import { signOut } from 'firebase/auth';
+import { View, Text, ScrollView, Alert, TouchableOpacity } from 'react-native';
 export default function ProfileScreen() {
+  const router = useRouter();
+
+  async function handleSignOut() {
+    try {
+      await signOut(auth); // Chama a função de logout do Firebase
+      console.log('Usuário deslogado com sucesso!');
+      
+      router.replace('/(auth)'); // Exemplo de navegação manual (menos ideal)
+
+    } catch (error) {
+      console.error('Erro ao fazer logout:', error);
+      Alert.alert('Erro', 'Não foi possível fazer logout. Verifique sua conexão e tente novamente.');
+    }
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text className="text-2xl font-bold text-white">Tela de Perfil</Text>
+    <View className="flex-1 bg-gray-100 dark:bg-black px-6 pt-6 pb-12 gap-4">
+      <ScrollView
+        className="flex-1"
+        contentContainerStyle={{ paddingBottom: 100 }} // Espaço para o botão Salvar não cobrir conteúdo
+        keyboardShouldPersistTaps="handled" // Fecha teclado ao tocar fora
+      >
+        <View className="gap-4">
+          <Text className="text-4xl font-extralight text-black dark:text-white">
+            Profile
+          </Text>
+          <TouchableOpacity
+            className="bg-red-200 dark:bg-red-800 p-3 rounded-full"
+            onPress={handleSignOut}
+          >
+            <Text className="text-black dark:text-white text-base text-center font-bold uppercase tracking-wider">
+              Logout
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </View>
   );
 }
